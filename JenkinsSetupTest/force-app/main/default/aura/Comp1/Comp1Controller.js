6b626b5bb5cf18a4b38c86dc6f240c58a455163b
Goyal <segoyal@deloitte.com>
2019-05-07 13:26:52 +0530

    Push

--- JenkinsSetupTest/force-app/main/default/aura/Comp1/Comp1Controller.js
+++
@@ -1,6 +0,0 @@
-({
-	myAction : function(component, event, helper) {
-        var c = component.get("v.abc");
-    console.log(c);
-	}
-})
--- JenkinsSetupTest/force-app/main/default/aura/LearningLightningOne/LearningLightningOne.app
+++
@@ -1,3 +0,0 @@
-<aura:application extends="force:slds">
-    <c:Comp1/>
-</aura:application>
